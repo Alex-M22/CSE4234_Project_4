c371cd7dfa4df96e0797da9f56b470dff14ddba3
Temp Usr <rodgerst2021@my.fit.edu>
2024-12-01 08:49:33 -0500

    Can now build without directory error.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './components/App/App.jsx'
-import data from "./assets/data/movies.json"
 
 createRoot(document.querySelector('main')).render(
   
